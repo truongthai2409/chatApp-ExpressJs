c9c0abe73c4e47b614556cb687500acea766ac62
Truong Thai <52000714@student.tdtu.edu.vn>
2023-11-12 22:59:01 +0700

    refactor: Refactor html css js pages login

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,7 +1,30 @@
-import { Application } from '@splinetool/runtime';
+import { Application } from "@splinetool/runtime";
+// let container = document.querySelector('.container');
 
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/XEjGPs-Hr5FE5giO/scene.splinecode');
-// <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.473/build/spline-viewer.js"></script>
-{/* <spline-viewer loading-anim url="https://prod.spline.design/XEjGPs-Hr5FE5giO/scene.splinecode"></spline-viewer> */}
+document.addEventListener("DOMContentLoaded", function () {
+  function showLoading() {
+    document.getElementById("loadingData").style.display = "block";
+  }
+
+  function hideLoading() {
+    document.getElementById("loadingData").style.display = "none";
+  }
+
+  function fetchingData() {
+    showLoading();
+    const canvas = document.getElementById("canvas3d");
+    const app = new Application(canvas);
+    app.load("https://prod.spline.design/XEjGPs-Hr5FE5giO/scene.splinecode");
+    const promise = app.load(
+      "https://prod.spline.design/XEjGPs-Hr5FE5giO/scene.splinecode"
+    );
+    promise.then(() => {
+      if (promise.finally.name == "finally") {
+        hideLoading();
+      }
+    });
+  }
+  
+  fetchingData();
+  // showLoading();
+});
